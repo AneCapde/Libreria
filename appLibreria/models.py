5fdcb2116a2aeb9e0c2056491046d3d7188def14
Jon Díaz <jondi@opendeusto.es>
2020-11-19 09:00:33 +0100

    Relaciones

--- appLibreria/models.py
+++ appLibreria/models.py
@@ -8,9 +8,19 @@ class Autor(models.Model):
     def str(self):
         return self.nombre
 
+
+class Tag(models.Model):
+    descripcion = models.CharField(max_length=25)
+
+    def str(self):
+        return self.descripcion
+
 class Coleccion(models.Model):
     nombreColeccion = models.CharField(max_length=35)
     primeraPublicacion = models.DateField()
+    autores = models.ManyToManyField(Autor)
+    tags = models.ManyToManyField(Tag)
+
     ESTADOS = (
         ('FIN', 'Finalizado'),
         ('ENCURSO','En publicación'),
@@ -20,19 +30,13 @@ class Coleccion(models.Model):
     estado = models.CharField(choices=ESTADOS, max_length=40)
 
     def str(self):
-        return self.nombre
+        return self.nombreColeccion
 
 class Comic(models.Model):
     nombreComic = models.CharField(max_length=35)
     numPaginas = models.IntegerField()
 
-
-
-    def str(self):
-        return self.nombre
-
-class Tag(models.Model):
-    descripcion = models.CharField(max_length=25)
+    autor = models.name = models.ForeignKey(Coleccion, on_delete=models.CASCADE, default=0)
 
     def str(self):
-        return self.nombre
+        return self.nombreComic
