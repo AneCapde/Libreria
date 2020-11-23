6f0ac6f3597fcb6701193667075ae7c4742a3729
Jon Díaz <jondi@opendeusto.es>
2020-11-23 20:20:57 +0100

    No se que he cambiado

--- appLibreria/models.py
+++ appLibreria/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Autor(models.Model):
     nombre = models.CharField(max_length=25)
+    anoNacimiento = models.IntegerField()
     # esto es una prueba3
     # esto es una prueba cuatro
     def str(self):
