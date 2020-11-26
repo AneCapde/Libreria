4ffa6c296ee4e5060de8648df851866e7231f135
AneCapde <anecapdevila@gmail.com>
2020-11-26 08:29:50 +0100

    index y css

--- appLibreria/views.py
+++ appLibreria/views.py
@@ -1,20 +1,20 @@
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from  .models import Autor, Coleccion,  Comic, Tag
 
-class Index(ListView):
+class Index(TemplateView):
 	template_name = 'index.html'
-	queryset = get_list_or_404(Coleccion.objects.all())
+	# queryset = get_list_or_404(Coleccion.objects.all())
 
-class Coleccion(DetailView):
-	template_name = 'coleccion.html'
-	queryset = get_list_or_404(Coleccion.objects.all())
+# class Coleccion(DetailView):
+# 	template_name = 'coleccion.html'
+# 	queryset = get_list_or_404(Coleccion.objects.all())
 
-	def get_context_data(self, **kwargs):
-		context = super().get_context_data(**kwargs)
-		id_ = self.kwargs.get("pk")
-		coleccion = Coleccion.objects.get(pk=id_)
-		context['tags_coleccion'] =  coleccion.Tags.all()
-		context['autores_coleccion'] =  coleccion.Autors.all()
-		return context
+	# def get_context_data(self, **kwargs):
+	# 	context = super().get_context_data(**kwargs)
+	# 	id_ = self.kwargs.get("pk")
+	# 	coleccion = Coleccion.objects.get(pk=id_)
+	# 	context['tags_coleccion'] =  coleccion.Tags.all()
+	# 	context['autores_coleccion'] =  coleccion.Autors.all()
+	# 	return context
