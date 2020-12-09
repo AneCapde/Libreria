a0639e23b5b877d27878256f435e722698a3211c
AneCapde <anecapdevila@gmail.com>
2020-12-09 16:22:44 +0100

    tagsList para un solo tag

--- appLibreria/static/js/tags.js
+++ appLibreria/static/js/tags.js
@@ -1,9 +1,30 @@
+var seleccionados = [];
+
 function updateResult(){
+    seleccionados = [];
+    hideEverything();
+    $('#form_categorias :checked').each(function () { 
+         seleccionados.push($(this).val());
+    });
+    if (seleccionados.length == 0){
+        return;
+    }else if (seleccionados.length == 1){
+        var id = '#'+seleccionados[0];
+        $(id).removeClass('hide');
+    }
+}
 
-    var fantasia =$('#Fantasia :checked').val();
-    var aventura =$('#Aventura :checked').val();
-    var romantica =$('#Romantica :checked').val();
-    var historica =$('#Historica :checked').val();
-
-    alert('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII. :p');
+function hideEverything(){
+   if(! $('#Fantasia').hasClass('hide')){
+    $('#Fantasia').addClass('hide');
+   }
+   if(! $('#Aventura').hasClass('hide')){
+    $('#Aventura').addClass('hide');
+   }
+   if(! $('#Romantica ').hasClass('hide')){
+    $('#Romantica ').addClass('hide');
+   }
+   if(! $('#Historica ').hasClass('hide')){
+    $('#Historica ').addClass('hide');
+   }
 }
