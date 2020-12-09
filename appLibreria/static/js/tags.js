2d3dcbbe4f638d4510ef0b8113cbc8ccd1948c58
AneCapde <anecapdevila@gmail.com>
2020-12-09 12:54:53 +0100

    TRABAJANDO CON TAGSLIST

---
+++ appLibreria/static/js/tags.js
@@ -0,0 +1,9 @@
+function updateResult(){
+
+    var fantasia =$('#Fantasia :checked').val();
+    var aventura =$('#Aventura :checked').val();
+    var romantica =$('#Romantica :checked').val();
+    var historica =$('#Historica :checked').val();
+
+    alert('HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII. :p');
+}
