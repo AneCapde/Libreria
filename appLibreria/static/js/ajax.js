b6f464fa5da18609c3f6fb1498765dbcca7e6e93
Jon Díaz <jondi@opendeusto.es>
2020-12-23 11:35:50 +0100

    Nada de ajax funciona :(

--- appLibreria/static/js/ajax.js
+++ appLibreria/static/js/ajax.js
@@ -1,14 +1,26 @@
-window.onload = $(document).ready(function() {
+window.onload =  $(document).ready(function() {
 
-   document.getElementById("fotoPagina").onmouseover = muestraMensaje;
-   
-   var pagina = document.getElementById("fotoPagina");
-   href = '127.0.0.1:8000/appLibreria/comic/'
+   $('body > div.container > div.content > section.paginas > p').each(function () {
+     var href = $(this).attr("href");
+     href = '127.0.0.1:8000/appLibreria/comic/1/0'/
+     $(this).qtip({
+        content: {
+           url: href,
+           method: 'foto2'
+        }
+     });
+   });
+ });	
+/** 
+   document.getElementById("#fotoPagina").onmouseover = muestraMensaje;
+
+
+   var pagina = document.getElementById("#fotoPagina");
+   href = '127.0.0.1:8000/appLibreria/comic/1/0'
    $(pagina).qtip({
       content: {
          url: href,
          method: 'get'
       }
    });
-
-  });
+*/
